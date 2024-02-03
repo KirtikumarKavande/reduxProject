bf0bad38844503e1c578db2aad45f7da899e1a0c
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-02-03 15:55:53 +0530

    normal and segregated data

---
+++ storingData/normalData.js
@@ -0,0 +1,44 @@
+const data=[
+    {
+        id:"1",
+        name:"kirtikumar",
+        todo:[
+            {
+                id:"1",
+                todoName:"playing cricket"
+            },
+            {
+                id:"1",
+                todoName:"learning  react-query"
+            },
+        ]
+    },
+    {
+        id:"2",
+        name:"sachin",
+        todo:[
+            {
+                id:"1",
+                todoName:"playing baseball"
+            },
+            {
+                id:"1",
+                todoName:"practicing song"
+            },
+        ]
+    },
+    {
+        id:"3",
+        name:"Rutuja",
+        todo:[
+            {
+                id:"1",
+                todoName:"system design electrical"
+            },
+            {
+                id:"1",
+                todoName:"watching movie song"
+            },
+        ]
+    },
+]
---
+++ storingData/segrigatedData.js
@@ -0,0 +1,45 @@
+const person = [
+  {
+    id: "1",
+    name: "kirtikumar",
+  },
+  {
+    id: "2",
+    name: "sachin",
+  },
+  {
+    id: "3",
+    name: "Rutuja",
+  },
+];
+
+const todos = [
+  {
+    userId: 1,
+    id: "1",
+    todoName: "playing cricket",
+  },
+  {
+    userId: 1,
+    id: "1",
+    todoName: "learning  react-query",
+  },
+  {
+    userId: 2,
+    id: "1",
+    todoName: "playing baseball",
+  },
+  { userId: 2, id: "2", todoName: "practicing song" },
+  {
+    userId: 3,
+    id: "1",
+    todoName: "system design electrical",
+  },
+  {
+    userId: 3,
+
+    id: "1",
+    todoName: "watching movie song",
+  },
+];
+
