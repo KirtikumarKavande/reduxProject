b6fbd6247294c3bba1148d848917768b16f8196a
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-02-03 09:30:00 +0530

    bindActionCreators

--- createStore/createStore.js
+++ createStore/createStore.js
@@ -1,17 +1,21 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 
 const reducerFunc = (state, action) => {
   if (action.type === "ADD_ITEM") {
-    return [...state, { name: action.name }];
+    return [...state, { name: action.name, quantity: action.quantity }];
   }
- // Default case
+  // Default case
   return state;
 };
-
+const addItem = (name, quantity) => ({
+  type: "ADD_ITEM",
+  name: name,
+  quantity: quantity ? quantity : 1,
+});
 const initialState = [{ name: "apple" }, { name: "rice" }];
+
 const response = createStore(reducerFunc, initialState);
-console.log(response.getState());
-response.dispatch({
-   type:"ADD_ITEM",name:"banana"
-})
-console.log(response.getState());
+
+const action = bindActionCreators({ addItem: addItem }, response.dispatch);
+action.addItem("baseBall",2)
+console.log(response.getState())
